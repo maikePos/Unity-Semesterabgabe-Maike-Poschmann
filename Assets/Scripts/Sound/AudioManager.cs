984e04f65e2a6f25e73333b02e30a6772fe90c23
Maike Poschmann <poschi1305@googlemail.com>
2024-01-09 20:33:42 +0100

    Background Music

---
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip walking;
+    public AudioClip mushrooms;
+    public AudioClip recordPlayer;
+
+    private void Start()
+    {
+        musicSource.clip = background;  
+        musicSource.Play();
+    }
+}
